0253c07ef9a7fb703d6c25f3ef4ad1f2d9dadc98
rizzlerCami <ctate624@canon-slade.bolton.sch.uk>
2024-12-04 17:51:27 +0000

    s

--- The Meat Man/Code/script.js
+++ The Meat Man/Code/script.js
@@ -1,9 +1,12 @@
 let floor, meatMan
 function setup() {
   new Canvas(1000, 1000)
+  world.gravity.y = 9.8
+
   floor = new Sprite(500, 900, 1000, 300, "s")
-  meatMan = new Sprite()
-  meatMan.image = ""
+  
+  meatMan = new Sprite(200, 800, 300, 300)
+  meatMan.image = `theMeatManRight.png`
 }
 function draw() {
   clear()
