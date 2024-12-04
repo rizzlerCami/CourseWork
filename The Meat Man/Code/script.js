513c547ece55019d8db2cef98358975e2f3f6fda
rizzlerCami <ctate624@canon-slade.bolton.sch.uk>
2024-12-04 18:44:48 +0000

    ghj

--- The Meat Man/Code/script.js
+++ The Meat Man/Code/script.js
@@ -1,4 +1,9 @@
-let floor, player
+let floor, player, faceR
+
+function preload() {
+  faceR = loadImage('theMeatManRight.png')
+  faceL = loadImage('The Meat Man Left.png')
+}
 
 function setup() {
   new Canvas(1000, 1000)
@@ -6,10 +11,16 @@ function setup() {
 
   floor = new Sprite(500, 900, 1000, 300, "s")
   
-  meatMan = new Sprite(200, 800, 300, 300)
-  meatMan.image = `theMeatManRight.png`
-  meatMan.image.scale = 0.2
+  meatMan = new Sprite(200, 800, 93, 100)
+  meatMan.image = faceR
+  meatMan.image.scale = 0.25
 }
+
 function draw() {
   clear()
+  if (kb.presses('left')) {
+    meatMan.image = faceL
+  } else if (kb.presses('right')) {
+    meatMan.image = faceR
+  }
 }
