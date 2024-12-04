bd0600c298c9db5436aeb182f872490aba95585f
rizzlerCami <ctate624@canon-slade.bolton.sch.uk>
2024-12-04 17:44:47 +0000

    f

--- The Meat Man/Code/script.js
+++ The Meat Man/Code/script.js
@@ -1,16 +1,7 @@
-let smiley;
+let floor
 function setup() {
-    new Canvas(500, 120);
-    let smileText = `
-..yyyyyy
-.yybyybyy
-yyyyyyyyyy
-yybyyyybyy
-.yybbbbyy
-..yyyyyy`;
-    smiley = new Sprite();
-    smiley.img = spriteArt(smileText, 32);
+
 }
 function draw() {
-  noLoop();
+  clear()
 }
