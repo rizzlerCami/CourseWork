f3a0346a6e8decd11508c165232e0583bc43999e
rizzlerCami <ctate624@canon-slade.bolton.sch.uk>
2025-01-09 20:33:29 +0000

    cold background

--- The Meat Man/Code/script.js
+++ The Meat Man/Code/script.js
@@ -1,13 +1,26 @@
-let meatMan
+let meatMan, sky, cloudB, cloudM, cloudF, FMount, GMount, grass
 
 function preload() {
-  
+  sky = loadImage('sky_fc.png')
+  cloudB = loadImage('clouds_mid_t_fc.png')
+  cloudM = loadImage('clouds_mid_fc.png')
+  cloudF = loadImage('clouds_front_t_fc.png')
+  FMount = loadImage('far_mountains_fc.png')
+  GMount = loadImage('grassy_mountains_fc.png')
+  grass = loadImage('Ground.png')
 }
 
 function setup() {
-  
+  new Canvas(windowWidth, windowHeight, "pixelated")
 }
 
 function draw() {
   clear()
-}
+
+  image(sky, 0, 0, windowHeight * 16/9, windowHeight)
+  image(FMount, 0, 0, windowHeight * 16/9, windowHeight)
+  image(GMount, 0, 0, windowHeight * 16/9, windowHeight)
+  image(cloudB, 0, 0, windowHeight * 16/9, windowHeight)
+  image(cloudM, 0, 0, windowHeight * 16/9, windowHeight)
+  image(cloudF, 0, 0, windowHeight * 16/9, windowHeight)
+} 
