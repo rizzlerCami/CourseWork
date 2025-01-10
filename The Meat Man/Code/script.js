eb7c607c0f8ee8545faba09b9cff7e017ef34d10
rizzlerCami <ctate624@canon-slade.bolton.sch.uk>
2025-01-10 12:54:04 +0000

    meatman put on

--- The Meat Man/Code/script.js
+++ The Meat Man/Code/script.js
@@ -1,5 +1,5 @@
-let meatMan, sky, cloudB, cloudM, cloudF, FMount, GMount, grass
-
+let meatMan, sky, cloudB, cloudM, cloudF, FMount, GMount, grass, ground, underImg, floor
+let v
 function preload() {
   sky = loadImage('sky_fc.png')
   cloudB = loadImage('clouds_mid_t_fc.png')
@@ -8,19 +8,33 @@ function preload() {
   FMount = loadImage('far_mountains_fc.png')
   GMount = loadImage('grassy_mountains_fc.png')
   grass = loadImage('Ground.png')
+  faceR = loadImage('theMeatManRight.png')
+  ground = loadImage('Ground.png')
+  underImg = loadImage('underGround.png')
 }
 
 function setup() {
   new Canvas(windowWidth, windowHeight, "pixelated")
+
+  meatMan = new Sprite([[188, 677], [147, 677], [127, 650], [127, 583], [213, 583], [213, 650], [188, 677]])
+  meatMan.image = faceR
+  meatMan.rotationLock = true
+  meatMan.mass = 30
+  meatMan.bounciness = 0
+  meatMan.friction = 500
+  v = 0.24
 }
 
 function draw() {
   clear()
 
-  image(sky, 0, 0, windowHeight * 16/9, windowHeight)
-  image(FMount, 0, 0, windowHeight * 16/9, windowHeight)
-  image(GMount, 0, 0, windowHeight * 16/9, windowHeight)
-  image(cloudB, 0, 0, windowHeight * 16/9, windowHeight)
-  image(cloudM, 0, 0, windowHeight * 16/9, windowHeight)
-  image(cloudF, 0, 0, windowHeight * 16/9, windowHeight)
+  image(sky, 0, 0, 900 * 16/9, 900)
+  image(FMount, 0, 0, 900 * 16/9, 900)
+  image(GMount, 0, 0, 900 * 16/9, 900)
+  image(cloudB, 0, 0, 900 * 16/9, 900)
+  image(cloudM, 0, 0, 900 * 16/9, 900)
+  image(cloudF, 0, 0, 900 * 16/9, 900)
+  meatMan.image.scale = v
+  meatMan.image.offset.y = -30
+  meatMan.image.offset.x = 4
 } 
