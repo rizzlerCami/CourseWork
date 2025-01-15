59c95947ee117be840bee479bd040fdcdf367e7c
rizzlerCami <ctate624@canon-slade.bolton.sch.uk>
2025-01-15 15:07:40 +0000

    dhfg

--- The Meat Man/Code/script.js
+++ The Meat Man/Code/script.js
@@ -1,16 +1,23 @@
-
+let meatMan
+let level = [true, false, false, false, false, false, false]
+let idle
+let scaleF
 
 function preload() {
-  
+  idle = loadImage('idle.png')
 }
 
 function setup() {
   new Canvas(windowWidth, windowHeight)
   world.gravity.y = 9.8
 
+  let meatMan = new Sprite([[388, 747], [347, 747], [327, 720], [327, 653], [413, 653], [413, 720], [388, 747]])
+  meatMan.image = idle
+  scaleF = 0.24
 }
 
 function draw() {
-  
   clear()
+  
+  meatMan.image.scale = scaleF
 }
